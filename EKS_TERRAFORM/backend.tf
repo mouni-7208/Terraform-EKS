14756e34b6a960757bf3626aa7ace9ecab1893b6
mouni-7208 <103107036+mouni-7208@users.noreply.github.com>
2024-02-02 13:05:36 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "bonam-s3bucket" # Replace with your actual S3 bucket name
+    bucket ="bonam-s3bucket" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
