f22d71a5003d8f0887d0f4a9c51081118aa9802b
mouni-7208 <103107036+mouni-7208@users.noreply.github.com>
2024-02-07 13:04:44 +0530

    Update main.tf

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -12,7 +12,7 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role" "example" {
-  name               = "eks-cluster-cloud3"
+  name               = "eks-cluster-cloud4"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
