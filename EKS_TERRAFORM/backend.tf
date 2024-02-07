e41261c5846b6996f04929e8389b6a209f10c372
mouni-7208 <103107036+mouni-7208@users.noreply.github.com>
2024-02-07 12:54:48 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket ="bhavi-s3bucket" # Replace with your actual S3 bucket name
+    bucket ="hotstar-buckets3" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-2"
+    region = "ap-south-1"
   }
 }
